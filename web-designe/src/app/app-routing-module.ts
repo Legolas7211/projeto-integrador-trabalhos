399c559d9ee608083aac582ec92e369212f627cc
Gabriel Evangelista D. <155906646+Legolas7211@users.noreply.github.com>
2025-09-02 23:42:41 +0000

    gerando e criando rotas de produos-module e shared-module 02.09.2025

--- web-designe/src/app/app-routing-module.ts
+++ web-designe/src/app/app-routing-module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModuleModule } from './shared-module/shared-module-module';
+import { ProdutosModuleModule } from './produtos-module/produtos-module-module';
 
 const routes: Routes = [
 
   {
     path: 'estrutura',
     loadChildren: () =>import('./estrutura/estrutura-module').then (m =>m.EstruturaModule)
+  },
+  {
+    path: 'shared-module',
+    loadChildren: () =>import('./shared-module/shared-module-module').then (m =>SharedModuleModule)
+  },
+  {
+    path: 'lista',
+    loadChildren: () =>import('./produtos-module/produtos-module-module').then (m =>ProdutosModuleModule)
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+ 
---
+++ web-designe/src/app/produtos-module/produtos-module-module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProdutosModuleRoutingModule } from './produtos-module-routing-module';
+import { ListaProdutosComponent } from './lista-produtos-component/lista-produtos-component';
+import { BotaoComponent } from '../shared-module/botao-component/botao-component';
+import { SharedModuleModule } from '../shared-module/shared-module-module';
+
+
+@NgModule({
+  declarations: [
+    ListaProdutosComponent
+  ],
+  imports: [
+    CommonModule,
+    ProdutosModuleRoutingModule,
+    SharedModuleModule
+  ]
+})
+export class ProdutosModuleModule { }
