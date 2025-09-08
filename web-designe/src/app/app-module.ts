1c284e3157abca5f2eacbe02b09280f67395db65
Gabriel Evangelista D. <155906646+Legolas7211@users.noreply.github.com>
2025-09-08 23:46:27 +0000

    Adicionando toolbar ao app html

--- web-designe/src/app/app-module.ts
+++ web-designe/src/app/app-module.ts
@@ -1,5 +1,11 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -10,7 +16,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
